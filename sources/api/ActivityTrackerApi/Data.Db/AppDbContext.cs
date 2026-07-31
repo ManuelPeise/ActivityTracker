751bf5db35578b09d8f508e046d54d8b56367bcd
manuel <manuel.p80@gmx.de>
2026-07-31 20:58:48 +0200

    configure new entities

--- sources/api/ActivityTrackerApi/Data.Db/AppDbContext.cs
+++ sources/api/ActivityTrackerApi/Data.Db/AppDbContext.cs
@@ -51,6 +51,90 @@ namespace Data.Db
                       .HasForeignKey<UserEntity>(u => u.UserAuthenticationId)
                       .OnDelete(DeleteBehavior.Cascade);
             });
+
+            modelBuilder.Entity<HealthConnectConfigurationEntity>(entity =>
+            {
+                entity.ToTable("HealthConnectConfiguration");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(e => e.DeviceId).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.DeviceName).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.Status).IsRequired();
+                entity.Property(e => e.IsActive).IsRequired();
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).IsRequired();
+                entity.Property(e => e.UserId).IsRequired();
+
+                entity.HasOne(e => e.User)
+                      .WithMany()
+                      .HasForeignKey(e => e.UserId)
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(e => e.HealthConnectSourceMappings)
+                      .WithOne()
+                      .OnDelete(DeleteBehavior.Cascade);
+
+                entity.HasMany(e => e.HealthConnectMetricMappings)
+                      .WithOne()
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<HealthConnectMetricEntity>(entity =>
+            {
+                entity.ToTable("HealthConnectMetricTable");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).IsRequired();
+            });
+
+            modelBuilder.Entity<HealthConnectMetricMappingEntity>(entity =>
+            {
+                entity.ToTable("HealthConnectMetricMapping");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(e => e.Source).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.DisplayName).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.IsGranted).IsRequired();
+                entity.Property(e => e.IsActive).IsRequired();
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).IsRequired();
+                entity.Property(e => e.MetricId).IsRequired();
+
+                entity.HasOne(e => e.MetricEntity)
+                      .WithMany()
+                      .HasForeignKey(e => e.MetricId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<HealthConnectSourceEntity>(entity =>
+            {
+                entity.ToTable("HealthConnectSourceTable");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).IsRequired();
+            });
+
+            modelBuilder.Entity<HealthConnectSourceMappingEntity>(entity =>
+            {
+                entity.ToTable("HealthConnectSourceMapping");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(e => e.Source).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.DisplayName).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.IsActive).IsRequired();
+                entity.Property(e => e.CreatedBy).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.CreatedAt).IsRequired();
+                entity.Property(e => e.SourceId).IsRequired();
+
+                entity.HasOne(e => e.SourceEntity)
+                      .WithMany()
+                      .HasForeignKey(e => e.SourceId)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
         }
     }
 }
